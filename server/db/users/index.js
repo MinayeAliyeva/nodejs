cc35d1f9c95ae6e314c8ef70a427b269be55e056
MinayeAliyeva <7s346pu@code.edu.az>
2024-04-30 00:50:35 +0400

    what is node

---
+++ server/db/users/index.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    name: "Minaya",
+    sname: "Aliyeva",
+  },
+  {
+    name: "Emin",
+    sname: "Aliyev",
+  },
+  {
+    name: "Nermin",
+    sname: "Alekberova",
+  },
+];
